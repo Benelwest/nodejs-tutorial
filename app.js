868c94ef9050e18f0cf08053024f56bba2ccd83b
ben <benell@bluewin.ch>
2020-05-23 21:09:04 +0200

    initial

---
+++ app.js
@@ -0,0 +1,7 @@
+const tutorial = require('./tutorial');
+//console.log(tutorial);
+
+console.log(tutorial.sum(1,1));
+console.log(tutorial.PI);
+console.log(new tutorial.SomeMathObject());
+//sum (1,1);
---
+++ app3.js
@@ -0,0 +1,6 @@
+const readline = require('readline');
+const rl = readline.createInterface({input : process.stdin,
+
+});
+
+rl.question(`What is $ {num1} + $`)
